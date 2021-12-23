434e2d22a375152e6c230ae812c7023aa142e1ca
unknown <sonykarl14@gmail.com>
2021-12-23 13:41:42 +0300

    added minor details

---
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/Models/orders/OrderItem.kt
@@ -0,0 +1,14 @@
+package info.dropy.dropy.Shops.commons.data.Models.orders
+
+import info.dropy.dropy.Shops.commons.data.Models.products.Product
+import javax.persistence.*
+
+@Entity
+data class OrderItem(
+    @Id
+    val id: Long,
+    @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val product: Product,
+    @Column(name = "quantity")
+    val quantity: Int
+)
--- src/main/kotlin/info/dropy/dropy/Shops/commons/data/Models/orders/Orders.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/Models/orders/Orders.kt
@@ -1,4 +1,19 @@
 package info.dropy.dropy.Shops.commons.data.Models.orders
 
-class Orders {
-}
+import info.dropy.dropy.Customers.data.models.Customer
+import info.dropy.dropy.Shops.commons.data.Models.shop.Shop
+import javax.persistence.*
+
+@Entity
+data class Orders (
+    @Id
+    val Id: Long,
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val customer: Customer,
+    @Column(name = "orderdate")
+    val orderDate: String,
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val shop: Shop,
+    @OneToMany(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    val OrderItems: List<OrderItem>?
+        )
