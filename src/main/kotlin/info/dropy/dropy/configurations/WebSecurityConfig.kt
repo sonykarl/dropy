bb7144eda6dda05f26a49eed4f089e23eb8b21d2
unknown <sonykarl14@gmail.com>
2022-01-18 10:51:45 +0300

    added mvc configurer

--- src/main/kotlin/info/dropy/dropy/configurations/WebSecurityConfig.kt
+++ src/main/kotlin/info/dropy/dropy/configurations/WebSecurityConfig.kt
@@ -1,12 +1,16 @@
 package info.dropy.dropy.configurations
 
 
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 import java.lang.reflect.Array.get
 import java.nio.file.Path
 import java.nio.file.Paths.get
 
+@EnableWebMvc
+@Configuration
 class WebSecurityConfig: WebMvcConfigurer{
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         exposeDirectory("shopsPhoto", registry)
