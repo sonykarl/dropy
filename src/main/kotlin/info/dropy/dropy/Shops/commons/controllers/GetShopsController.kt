3ed1f8479d7e0b78819902ebc72f984f4efc9c9b
unknown <sonykarl14@gmail.com>
2022-01-18 15:41:25 +0300

    fetched imgs

--- src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/GetShopsController.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/GetShopsController.kt
@@ -5,11 +5,17 @@ import info.dropy.dropy.Shops.commons.data.dtos.GetShopByCategoryDto
 import info.dropy.dropy.Shops.commons.services.ShopDetailsService
 import org.apache.tomcat.util.http.fileupload.IOUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.core.io.InputStreamResource
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.io.File
 import java.io.FileInputStream
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.servlet.http.HttpServletResponse
 
 @RestController
@@ -18,9 +24,11 @@ class GetShopsController @Autowired constructor(
     private val shopDetailsService: ShopDetailsService
     ){
     @GetMapping("all")
-    fun getShops(response: HttpServletResponse){
+    fun getShops(response: HttpServletResponse): ResponseEntity<Any>{
         val inputStream = FileInputStream(File("C:\\Users\\wuodmogo\\IdeaProjects\\dropy\\dropy\\src\\main\\resources\\static\\shopslogoimageslabel.PNG")) //loading the file
-        IOUtils.copy(inputStream,response.outputStream)
-        response.flushBuffer()
+        val inputStreamResource = InputStreamResource(inputStream)
+        val header = HttpHeaders()
+        header.setContentLength(Files.size(Paths.get("C:\\Users\\wuodmogo\\IdeaProjects\\dropy\\dropy\\src\\main\\resources\\static\\shopslogoimageslabel.PNG")))
+        return ResponseEntity(inputStreamResource, header, HttpStatus.OK)
     }
 }
