d56c15509797e5c8fdde73b0242aed7691c846ce
unknown <sonykarl14@gmail.com>
2022-02-25 20:03:26 +0300

    removed

--- src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/GetShopsController.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/GetShopsController.kt
@@ -1,16 +1,9 @@
 package info.dropy.dropy.Shops.commons.controllers
 
-import com.mysql.cj.util.StringUtils.toString
-import info.dropy.dropy.Shops.commons.data.Models.shop.Shop
-import info.dropy.dropy.Shops.commons.data.dtos.GetShopByCategoryDto
-import info.dropy.dropy.Shops.commons.data.dtos.ShopDetailsDto
+
 import info.dropy.dropy.Shops.commons.services.ShopDetailsService
-import org.apache.tomcat.util.http.fileupload.IOUtils
+
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.InputStreamResource
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
