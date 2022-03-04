60ddda508d131658b1394bc7a6e86738bae04b3d
unknown <sonykarl14@gmail.com>
2022-03-04 19:23:01 +0300

    firebase id

--- src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/ShopRepo.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/ShopRepo.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface ShopRepo: JpaRepository<Shop,Long> {
-    fun findByfirebaseid(firebase_id: String?): Shop
+    fun findByfirebaseId(firebase_id: String?): Shop
     fun findByCategoryId(category: Long?): List<Shop>?
     fun findById(id: Long?): Shop?
 }
