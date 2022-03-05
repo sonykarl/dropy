eaa3c21e2cb3fc438634311ff5e13a4e00c1f67e
unknown <sonykarl14@gmail.com>
2022-03-05 11:56:39 +0300

    used JQL query

--- src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/ShopRepo.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/ShopRepo.kt
@@ -2,11 +2,15 @@ package info.dropy.dropy.Shops.commons.data.repositories.shops
 
 import info.dropy.dropy.Shops.commons.data.Models.shop.Shop
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ShopRepo: JpaRepository<Shop,Long> {
-    fun findByFirebaseId(firebase_id: String?): Shop?
+    @Query("FROM SHOP WHERE firebase_id = :firebase_id")
+    fun findByFirebaseId(
+        @Param("firebase_id") firebase_id: String?): Shop?
     fun findByCategoryId(category: Long?): List<Shop>?
     fun findById(id: Long?): Shop?
 }
