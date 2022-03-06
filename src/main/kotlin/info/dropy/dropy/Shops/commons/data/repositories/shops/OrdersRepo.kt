5835f2d75ad187c67e7bfc9beb0c0d40f171f55b
unknown <sonykarl14@gmail.com>
2022-03-06 20:23:09 +0300

    minor fixes

--- src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/OrdersRepo.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/repositories/shops/OrdersRepo.kt
@@ -4,12 +4,13 @@ import info.dropy.dropy.Customers.data.orders.OrderStatus
 import info.dropy.dropy.Shops.commons.data.Models.orders.Orders
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface OrdersRepo: JpaRepository<Orders, Long> {
     fun findAllByShopId(shopId:Long): List<Orders?>?
     fun findAllByCustomerId(customerId:Long): List<Orders?>?
-    @Query("SELECT * FROM ORDERS r WHERE r.status = :orderStatus AND shop_id = :shopId", nativeQuery = true)
-    fun findAllByStatusAndShopId(orderStatus: String, shopId: Long):List<Orders?>
+    @Query("SELECT * FROM ORDERS o WHERE o.status = :orderStatus AND o.shop_id = :shopId", nativeQuery = true)
+    fun findAllByStatusAndShopId(@Param("orderStatus")orderStatus: String, @Param("shopId")shopId: Long):List<Orders?>
 }
