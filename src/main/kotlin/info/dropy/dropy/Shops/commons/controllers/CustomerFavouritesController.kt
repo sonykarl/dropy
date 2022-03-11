3800ab86d20a20967c0105e5810dad08c547eacd
unknown <sonykarl14@gmail.com>
2022-03-12 00:12:06 +0300

    added followeres

--- src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/CustomerFavouritesController.kt
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/controllers/CustomerFavouritesController.kt
@@ -1,12 +1,11 @@
 package info.dropy.dropy.Shops.commons.controllers
 
+import info.dropy.dropy.Shops.commons.data.Models.shop.CustomerFavourites
+import info.dropy.dropy.Shops.commons.data.dtos.CustomerFavouritesDto
 import info.dropy.dropy.Shops.commons.services.CustomerFavouritesService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("api/v1/shops/favourites")
@@ -37,6 +36,18 @@ class CustomerFavouritesController @Autowired constructor(
         val favourites = customerFavouritesService.findAllFavourites()
         return ResponseEntity.ok()
             .body(favourites)
+    }
+
+    @PostMapping("addfavourites")
+    fun addCustomerFavourites(
+        @RequestBody body:CustomerFavouritesDto
+    ):ResponseEntity<Any>{
+        val favourite = CustomerFavourites(
+            customer = body.customer,
+            shop = body.shop
+        )
+        return  ResponseEntity.ok()
+            .body(favourite)
     }
 
 }
---
+++ src/main/kotlin/info/dropy/dropy/Shops/commons/data/dtos/CustomerFavouritesDto.kt
@@ -0,0 +1,6 @@
+package info.dropy.dropy.Shops.commons.data.dtos
+
+data class CustomerFavouritesDto (
+    val customer: Long,
+    val shop: Long
+        )
