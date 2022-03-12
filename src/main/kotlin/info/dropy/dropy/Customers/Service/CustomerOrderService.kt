f7ef0cd5be8608c9e9cce70332d9c898ea3a834f
unknown <sonykarl14@gmail.com>
2022-03-12 05:31:28 +0300

    dfdf

--- src/main/kotlin/info/dropy/dropy/Customers/Service/CustomerOrderService.kt
+++ src/main/kotlin/info/dropy/dropy/Customers/Service/CustomerOrderService.kt
@@ -2,10 +2,8 @@ package info.dropy.dropy.Customers.Service
 
 import info.dropy.dropy.Customers.data.orders.CustomerOrder
 import info.dropy.dropy.Customers.data.repositories.CustomerOrderRepo
-import jdk.internal.net.http.common.Log
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import java.io.IOException
 
 @Service
 class CustomerOrderService @Autowired constructor(
@@ -13,10 +11,8 @@ class CustomerOrderService @Autowired constructor(
 ) {
 
     fun addCustomerOrder(order: CustomerOrder){
-        try {
+
             orderrepo.save(order)
-        }catch (e:IOException){
-            Log.ALL
-        }
+
     }
 }
